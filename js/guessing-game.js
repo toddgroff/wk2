8d341a8d7b6afa4cd9243e9d64e6690ea2d5e3b5
Chris Davies <chris.davies@theironyard.com>
2015-01-20 21:31:16 -0500

    Adding the guessing game assignment

---
+++ js/guessing-game.js
@@ -0,0 +1,20 @@
+// TODO: Write a number guessing game.
+//
+// Rules: The game will generate a number between 1 and 10,
+// inclusive. So, it might generate a 1. Or it might generate
+// a 10, or any whole number in between.
+//
+// The user should enter a number into the text box and then
+// click "guess".
+//
+// You should then tell the user (via alert or another way)
+// if her guess was too big, too small, or just right!
+//
+//
+// Look at the HTML, and note that you'll need to
+// write two functions:
+//
+// setGuess(guessValueAsString)
+// guess()
+//
+// Feel free to rename the guessValueAsString parameter
